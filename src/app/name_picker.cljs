3fc73842ad6752cd47f58b33de027fcca62925d1
Capitalistlepton <CapitalistLepton@users.noreply.github.com>
2021-05-04 17:01:09 -0700

    Finalize skeleton of name-picker letter pad.

--- src/app/name_picker.cljs
+++ src/app/name_picker.cljs
@@ -3,25 +3,34 @@
             [app.lib :as lib]
             [reagent.core :as r]))
 
+(defonce name-picked (r/atom [:0]))
+
 (defn letter-elem
   [letter name]
   [:li
-   ; {:on-click (fn []
-   ;              (swap! name
-   ;                     (fn [current-name]
-   ;                       (conj current-name letter))))}
-   letter])
+   [:button
+    {:on-click (fn []
+                 (when (< (count @name) 3)
+                   (swap! name conj letter))
+                 (println @name))}
+    letter]])
 
 (defn letters-list
   "Makes list of letters"
   [name]
   [:ul {:id "letters"}
-       (for [letter lib/letters]
-         ^{:key letter} [letter-elem letter name])])
+   (for [letter lib/letters]
+     ^{:key letter} [letter-elem letter name])
+   [:li
+    [:button {:on-click (fn []
+                     (when (> (count @name) 0)
+                       (swap! name pop))
+                     (println @name))}
+     "Delete"]]])
 
 (defn draw-name-picker
   "Draw list of names"
   []
-  (let [name (r/atom [:0])]
-       [letters-list name]
-       [:p @name]))
+  [:div
+   "Name: " (str @name-picked)
+   [letters-list name-picked]])
