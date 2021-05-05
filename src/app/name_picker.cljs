1855f814d1c422bd38b93c5c0b8df1454ea2f589
Capitalistlepton <CapitalistLepton@users.noreply.github.com>
2021-05-04 17:34:35 -0700

    Display properties of the chosen name.

--- src/app/name_picker.cljs
+++ src/app/name_picker.cljs
@@ -3,34 +3,48 @@
             [app.lib :as lib]
             [reagent.core :as r]))
 
-(defonce name-picked (r/atom [:0]))
+(defonce name-picked (r/atom []))
 
 (defn letter-elem
-  [letter name]
+  [letter name-ref]
   [:li
    [:button
     {:on-click (fn []
-                 (when (< (count @name) 3)
-                   (swap! name conj letter))
-                 (println @name))}
+                 (when (< (count @name-ref) 3)
+                   (swap! name-ref conj letter))
+                 (println @name-ref))}
     letter]])
 
 (defn letters-list
   "Makes list of letters"
-  [name]
+  [name-ref]
   [:ul {:id "letters"}
    (for [letter lib/letters]
-     ^{:key letter} [letter-elem letter name])
+     ^{:key letter} [letter-elem letter name-ref])
    [:li
     [:button {:on-click (fn []
-                     (when (> (count @name) 0)
-                       (swap! name pop))
-                     (println @name))}
+                     (when (> (count @name-ref) 0)
+                       (swap! name-ref pop))
+                     (println @name-ref))}
      "Delete"]]])
+
+(defn name-properties
+  "Display properties for the 3 letter name that was picked"
+  [name]
+  (when (= (count name) 3)
+    (let [properties (lib/properties name)]
+      [:div
+       [:p "Properties for " (str name)]
+       [:p {:style {:color (:color properties)}}
+        "Color"]
+       [:p "Speed: " (:speed properties)]
+       [:p "Strength: " (:strength properties)]
+       [:p "Health: " (:health properties)]])))
 
 (defn draw-name-picker
   "Draw list of names"
   []
   [:div
    "Name: " (str @name-picked)
-   [letters-list name-picked]])
+   [letters-list name-picked]
+   [name-properties @name-picked]])
